32299b48c09655f4c1c3f8475c2c33d12836ec22
Ginger Griffis <gingergriffis@hotmail.com>
2013-11-28 13:01:32 -0800

    Fixed create admin  migration I hope

--- db/migrate/20131128205349_create_admin_user.rb
+++ db/migrate/20131128205349_create_admin_user.rb
@@ -1,4 +1,5 @@
-class CreateAdminUser < ActiveRecord::Migration  def migrate(direction)
+class CreateAdminUser < ActiveRecord::Migration
+  def migrate(direction)
     super
     # Create a default user
     AdminUser.create!(:email => (ENV["ADMIN_USERNAME"]).dup, :password => (ENV["ADMIN_PASSWORD"]).dup, :password_confirmation => (ENV["ADMIN_PASSWORD"]).dup) if direction == :up
